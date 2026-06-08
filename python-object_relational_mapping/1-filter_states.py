1ff90d6ba09334afab4cea6fe0f476b9077a2717
Collinsodiobi <Collinsodi10@gmail.com>
2026-06-08 11:58:35 +0000

    Fix pycodestyle line too long in 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N from the database."""
+"""Lists all states with a name starting with N from the database."""
 import MySQLdb
 import sys
 
