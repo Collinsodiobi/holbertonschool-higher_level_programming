b44226a3362bab66c362e2556ce67c84b7a47b7f
Collinsodiobi <Collinsodi10@gmail.com>
2026-06-10 10:00:15 +0000

    Fix Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Lists all states that match the name given as argument."""
+
 import MySQLdb
 import sys
 
@@ -13,12 +14,17 @@ if __name__ == "__main__":
         db=sys.argv[3],
         charset="utf8"
     )
+
     cur = conn.cursor()
+
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}'"
-        " ORDER BY id ASC".format(sys.argv[4])
+        "SELECT * FROM states WHERE name = '{}' "
+        "ORDER BY id ASC".format(sys.argv[4])
     )
-    for row in cur.fetchall():
+
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
+
     cur.close()
     conn.close()
