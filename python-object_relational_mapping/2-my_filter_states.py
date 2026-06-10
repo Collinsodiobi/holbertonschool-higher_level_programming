93846c2da44ffbeb0a165901b783848be7bbf61e
Collinsodiobi <Collinsodi10@gmail.com>
2026-06-10 10:07:31 +0000

    Fix Task 2 format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,12 +18,11 @@ if __name__ == "__main__":
     cur = conn.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' "
-        "ORDER BY id ASC".format(sys.argv[4])
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+        .format(sys.argv[4])
     )
 
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
